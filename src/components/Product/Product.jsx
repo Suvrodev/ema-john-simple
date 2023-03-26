75a27518a3a8ed89d826cb865d255e6afd9eb38f
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-03-27 02:31:51 +0600

    Display more data of Product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-   const {img,name,seller,quantity}= props.info
+   const {img,name,seller,price,ratings}= props.info
     return (
         <div className='product_container'>
             <img src={img} alt="" />
-             <h4>{name}</h4>
+             <h4 className='product_name' >{name}</h4>
+             <div className='product_info'>
+                <p>Price: {price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings} star </p>
+             </div>
+             <button className='btn_cart'>Add to Cart</button>
         </div>
     );
 };
